35d6bbc209280c75b81895ff845471d6bdd09ee8
Hermann Ouré <hermann.oure@gmail.com>
2020-09-21 19:35:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pg = require('pg');
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
-const app = express();
+var app = express();
 
 app.set('port', process.env.PORT || 5000);
 
